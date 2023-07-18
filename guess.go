37f228c67bdb15744702cec2f650b014e6f37bfb
Andrey Deryugin <andrey_deryugin_97@vk.com>
2023-07-18 21:03:22 +0300

    2. User input your digits

--- guess.go
+++ guess.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,4 +18,26 @@ func main() {
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
 	fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Make a guess: ")
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if guess < target {
+		fmt.Println("Oops. Your guess was LOW.")
+	} else if guess > target {
+		fmt.Println("Oops. Your guess was HIGH")
+	}
 }
